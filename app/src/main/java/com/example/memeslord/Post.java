3315fc4ecced5fe0ba66b6096d6e29752b173baa
Julien Percheron <julien_p@icloud.com>
2020-03-11 09:36:24 +0100

    Debut implementation API Reddit

---
+++ app/src/main/java/com/example/memeslord/Post.java
@@ -0,0 +1,57 @@
+package com.example.memeslord;
+
+public class Post {
+    private String title;
+    private String author;
+    private String postURL;
+    private String thumbnailURL;
+    private String imageURL;
+
+    public Post(String title, String author, String postURL, String thumbnailURL, String imageURL) {
+        this.title = title;
+        this.author = author;
+        this.postURL = postURL;
+        this.thumbnailURL = thumbnailURL;
+        this.imageURL = imageURL;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPostURL() {
+        return postURL;
+    }
+
+    public void setPostURL(String postURL) {
+        this.postURL = postURL;
+    }
+
+    public String getThumbnailURL() {
+        return thumbnailURL;
+    }
+
+    public void setThumbnailURL(String thumbnailURL) {
+        this.thumbnailURL = thumbnailURL;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}

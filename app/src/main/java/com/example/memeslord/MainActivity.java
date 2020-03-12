2048c1b80eca73552ad8f4433dd95a6899fe7e66
NilsCarron <58626121+NilsCarron@users.noreply.github.com>
2020-03-12 18:06:34 +0100

    Login
    Ta mère la pute nègre

--- app/src/main/java/com/example/memeslord/MainActivity.java
+++ app/src/main/java/com/example/memeslord/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,18 +14,22 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
 
+    private static final String TAG = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button Login = findViewById(R.id.buttonNewMeme);
 
+
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goNewMeme = new Intent(v.getContext(), NewMemeActivity.class);
-                startActivity(goNewMeme);
+                Intent Login = new Intent(v.getContext(), LoginActivity.class);
+                startActivity(Login);
             }
         });
     }
